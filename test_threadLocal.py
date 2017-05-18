a2dd2eab235307c78dd11d5f265f5672fb58d98c
syjs10 <350172898@qq.com>
2017-05-18 20:45:20 +0800

    learning_update

---
+++ test_threadLocal.py
@@ -0,0 +1,15 @@
+import threading
+
+local_school = threading.local()
+
+def process_student():
+	print ('Hello, %s! (This is %s)' % (local_school.student, threading.current_thread().name))
+def process_thread(name):
+	local_school.student = name
+	process_student()
+t1 = threading.Thread(target=process_thread, args=("Bob",), name="Thread-A")
+t2 = threading.Thread(target=process_thread, args=("JS",), name="Thread-B")
+t1.start()
+t2.start()
+t1.join()
+t2.join()
